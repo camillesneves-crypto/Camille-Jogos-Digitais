3a644716c2c677b3ee7d62a9b9412ec9793d4d33
Camille <camillesneves@gmail.com>
2026-07-25 21:22:12 -0300

    Listas 2

---
+++ Lista 02/Exercício 10/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+string escolhaÁrea;
+string evento;
+int nível;
+int possuiItem;
+int possuiAliado;
+
+Console.WriteLine("Escolha uma área:");
+Console.WriteLine("Floresta");
+Console.WriteLine("Ruínas");
+Console.WriteLine("Laboratório");
+Console.WriteLine("Praia"); 
+escolhaÁrea = (Console.ReadLine());
+
+switch (escolhaÁrea)
+{
+    case "Floresta": evento = "Um animal selvagem apareceu!";
+        Console.WriteLine(evento);
+        break;
+
+    case "Ruínas": evento = "Uma armadilha antiga foi ativada!";
+        Console.WriteLine(evento);
+        break;
+
+    case "Laboratório": evento = "Um robô descontrolado apareceu!";
+        Console.WriteLine(evento);
+        break;
+
+    case "Praia": evento = "Uma tempestade repentina começou!";
+        Console.WriteLine(evento);
+        break;
+
+    default: evento = "Opção inválida.";
+        Console.WriteLine(evento);
+        break;
+}
+
+Console.WriteLine("Informe o nível do seu personagem.");
+nível = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Você possui um item especial?");
+Console.WriteLine("1 - Sim");
+Console.WriteLine("2 - Não");
+possuiItem = int.Parse (Console.ReadLine());
+
+Console.WriteLine("Você está com algum aliado?");
+Console.WriteLine("1 - Sim");
+Console.WriteLine("2 - Não");
+possuiAliado = int.Parse (Console.ReadLine());
+
+if (nível>=5 && possuiItem == 1 || possuiAliado==1)
+{
+    Console.WriteLine("Área escolhida: " + escolhaÁrea );
+    Console.WriteLine("Evento: " + evento);
+    Console.WriteLine("Você resolveu este evento com sucesso!");
+}
+else
+{
+    Console.WriteLine("Área escolhida: " + escolhaÁrea);
+    Console.WriteLine("Evento: " + evento);
+    Console.WriteLine("Você não estava preparado para esse evento.");
+}
---
+++ Lista 02/Exercício 9/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+int escolha;
+int energia;
+
+Console.WriteLine("Escolha um tipo de ataque:");
+Console.WriteLine("1 - Ataque leve");
+Console.WriteLine("2 - Ataque pesado");
+Console.WriteLine("3 - Especial");
+Console.WriteLine("4 - Esquivar");
+escolha = int.Parse (Console.ReadLine());
+
+Console.WriteLine("Qual a quantidade de energia que você possui?");
+energia = int.Parse (Console.ReadLine());
+
+switch (escolha)
+{
+    case 1: Console.WriteLine("Você atacou leve!");
+        break;
+    case 2: if (energia >= 15)
+        {
+            Console.WriteLine("Você atacou pesado!");
+        }
+        else { Console.WriteLine("Você não possui energia suficiente para atacar."); }
+            break; 
+    case 3: if (energia >= 30)
+        {
+            Console.WriteLine("Você usou o especial!");
+        }
+    else
+        {
+            Console.WriteLine("Você não possui energia suficiente para usar o especial.");
+        }
+        break;
+    case 4: if (energia >=10)
+        {
+            Console.WriteLine("Você esquivou!");
+        }
+    else
+        {
+            Console.WriteLine("Você não possui energia suficiente para esquivar.");
+        }
+        break;
+    default: Console.WriteLine("Opção inválida.");
+        break; 
+
+}
